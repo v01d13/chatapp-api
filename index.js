ffae3cbe2f0b83f34feed39a6e04710b453a70e3
v01d13 <49680745+v01d13@users.noreply.github.com>
2021-02-02 01:16:58 +0545

    ...

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const app = require('express')()
-const http = require('http').createServer(app)
+const https = require('https').createServer(app)
 
 
 app.get('/', (req, res) => {
     res.send("Node Server is running. Yay!!")
-})
+});
 
 //Socket Logic
-const socketio = require('socket.io')(http)
+const socketio = require('socket.io')(https)
 
 socketio.on("connection", (userSocket) => {
     userSocket.on("send_message", (data) => {
         userSocket.broadcast.emit("receive_message", data)
     })
-})
+});
 
-http.listen(process.env.PORT)
+https.listen(process.env.PORT || 3000, function() {
+});
+console.log('Listening');
