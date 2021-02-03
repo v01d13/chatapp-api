92e0ffb544975dfabd3af24c4bfb954f458102e8
v01d13 <49680745+v01d13@users.noreply.github.com>
2021-02-03 13:40:56 +0545

    Port fix

--- index.js
+++ index.js
@@ -9,25 +9,25 @@ app.get('/', (req, res) => {
 });
 
 socketio.on('connection', (socket) => {
-    console.log('user connected')
-    socket.on('send_message', (data) => {
-      console.log(data);
-        socket.broadcast.emit("receive_message", data);
-    });
-    socket.on('chat', (msg) => {
-      console.log('message: ' + msg);
-    });
+  console.log('user connected')
+  socket.on('send_message', (data) => {
+    console.log(data);
+      socket.broadcast.emit("receive_message", data);
+  });
+  socket.on('chat', (msg) => {
+    console.log('message: ' + msg);
+  });
 });
 
 socketio.on('clientError', (err, socket) => {
   console.error(err);
   socketio.end('HTTP/1.1 400 Bad Request\r\n\r\n');
 });
+
 socketio.on('disconnect', () => {
   console.log(`disconnected`);
 });
 
-https.listen(4559, (req, res) => {
-  console.log(https.address());
+https.listen(process.env.PORT || 3000, (req, res) => {
+  console.log('Listening: ', https.address());
 });
-console.log('Listening');
