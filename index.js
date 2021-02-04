717e27bbd6cad3be5de99f16ca24cc1802f42167
v01d13 <49680745+v01d13@users.noreply.github.com>
2021-02-04 17:50:54 +0545

    Parse data

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // Importing modules
-var express = require('express');
-var app = express();
-var https = require('http').Server(app);
-var socketio = require('socket.io')(https);
-var mongoose = require('mongoose');
-var enforce = require('express-sslify');
+const express = require('express');
+const app = express();
+const https = require('http').Server(app);
+const socketio = require('socket.io')(https);
+const mongoose = require('mongoose');
+const enforce = require('express-sslify');
 //
 app.get('/', (req, res) => {
-  res.send("Node Server is test running!!");
+  res.send("Https test!!");
 });
 app.use(enforce.HTTPS());
 // Creating a promise, and message model for mongoose
 mongoose.Promise = Promise;
-var dbUrl = 'mongodb+srv://v01d13:wFYQrplPbOqDf6tG@chat-app-mongo.dqlry.mongodb.net/<dbname>?retryWrites=true&w=majority';
-var Message = mongoose.model('Message', {
+const dbUrl = 'mongodb+srv://v01d13:wFYQrplPbOqDf6tG@chat-app-mongo.dqlry.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const Message = mongoose.model('Message', {
   username: String,
   message: String
 });
@@ -25,7 +25,8 @@ socketio.on('connection', async (socket) => {
       console.error(err);
     else
       var json_parse = JSON.parse(data);
-      socket.emit('initial_message_load', json(json_parse));
+      console.log(json_parse);
+      socket.emit('initial_message', json(json_parse));
   };
 });
 // Socket connection error
