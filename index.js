b66d8cd08d3accc1bf287d523d98be39b7b8788a
v01d13 <49680745+v01d13@users.noreply.github.com>
2021-02-04 19:48:28 +0545

    .....

--- index.js
+++ index.js
@@ -21,8 +21,10 @@ const Message = mongoose.model('Message', {
 socketio.on('connection',  async (socket) => {
   console.log('User connected');
   try{
-    await Message.find({username: 'Suresh'}, (data) => {
+    await Message.find().lean({username: 'Suresh'}, (data) => {
       var json_parse = JSON.stringify(data);
+      console.log(typeof(data));
+      console.log(typeof(json_parse));
       console.log(json_parse);
       socket.emit(json_parse);
     })
